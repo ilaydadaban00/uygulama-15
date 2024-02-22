ff2064eb0bd02b7831e96605fd3efa5657e2cd94
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-23 01:03:43 +0300

    Add files via upload

---
+++ uygulama 15_/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace uygulama_15_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(179, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "fiyat";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFiyat.Location = new System.Drawing.Point(287, 61);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(157, 33);
+            this.txtFiyat.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Navy;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(133, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "indirim %10";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Navy;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(306, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 64);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "indirim %25";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Navy;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(133, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 64);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "indirim %50";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Navy;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(306, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 64);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "indirim %75";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ uygulama 15_/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_15_
+{
+    public partial class Form1 : Form
+    {
+        int etiketFiyati;
+        Double indirimliFiyat;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat=etiketFiyati-etiketFiyati*0.10;
+            MessageBox.Show(indirimliFiyat.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.25;
+            MessageBox.Show(indirimliFiyat.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.50;
+            MessageBox.Show(indirimliFiyat.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            etiketFiyati = Convert.ToInt32(txtFiyat.Text);
+            indirimliFiyat = etiketFiyati - etiketFiyati * 0.75;
+
+
+
+
+
+
+
+
+
+
+
+            MessageBox.Show(indirimliFiyat.ToString());
+        }
+    }
+}
